5c8abec9820e3527f9a01a929acd6c087f01d907
Luiza Carneiro <luizacc@yahoo.com>
2017-10-15 21:54:35 -0700

    Added more formatting to the template. Added field to be edited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>',
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div><label>name: </label>
+  <input [(ngModel)]="hero.name" placeholder="name">
+  </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
